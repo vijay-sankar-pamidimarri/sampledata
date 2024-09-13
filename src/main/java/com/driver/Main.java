de61f0ec2cf1f0eb334362e415725a6cb474c6fd
vijay-sankar-pamidimarri <sankerpamidimarri@gmail.com>
2024-09-13 09:37:56 +0530

    Add files via upload

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,17 @@
+package com.driver;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Task 3: Create an object of RWOnly class
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Try setting a value to the private member variable directly
+        // obj.name = "John";  // Error: 'name' has private access in RWOnly
+
+        // Task 4: Try printing the value directly
+        // System.out.println(obj.name);  // Error: 'name' has private access in RWOnly
+        obj.setName("vijay");
+        obj.getName();
+    }
+}
---
+++ src/main/java/com/driver/RWOnly.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class RWOnly {
+    // Task 2: Private member variable
+    private String name;  // name is private to restrict direct access
+
+    // Task 5: Getter and Setter for encapsulation
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
